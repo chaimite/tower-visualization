9c716a4bbd797029b484e5f0c5714b83ccd8699b
xaimite <adrianocorreiacesar@gmail.com>
2025-04-14 22:54:11 +0100

    feat: remove ngmodule

--- tower-visualization/src/app/tower/tower.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { TowerComponent } from './tower.component';
-
-@NgModule({
-  imports: [TowerComponent],   // Import the standalone component
-  exports: [TowerComponent]    // Export it for use in other modules
-})
-export class TowerModule {}
