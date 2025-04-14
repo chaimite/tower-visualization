cd51b65eb0d1f7359471178afed0407b759662c2
xaimite <adrianocorreiacesar@gmail.com>
2025-04-14 22:55:00 +0100

    feat: initialize service subscription

--- tower-visualization/src/app/tower/tower.component.ts
+++ tower-visualization/src/app/tower/tower.component.ts
@@ -1,28 +1,33 @@
 import { Component, NgModule, OnInit } from '@angular/core';
 import { Observable, take } from 'rxjs';
 import { selectAllTowers, selectCurrentTower } from './tower.selectors';
-import { selectTower } from './tower.actions';
+import { selectTower, setTowers } from './tower.actions';
 import { Store } from '@ngrx/store';
 import { Tower } from '../simulation/tower';
-import { AsyncPipe, CommonModule, NgFor, NgIf } from '@angular/common';
+import { AsyncPipe, CommonModule, JsonPipe, NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { TowerService } from '../simulation/tower.service';
 
 @Component({
   selector: 'app-tower',
   templateUrl: './tower.component.html',
   styleUrls: ['./tower.component.scss'],
-  imports: [CommonModule]
+  standalone: true,
+  imports: [CommonModule, FormsModule, NgIf, NgFor, AsyncPipe],
+  providers: [TowerService]
 })
 export class TowerComponent implements OnInit {
   towers$: Observable<Tower[]>;
   selectedTower$: Observable<Tower>;
 
-  constructor(private store: Store) {
+  constructor(private store: Store, private towerService: TowerService) {
     this.towers$ = this.store.select(selectAllTowers);
     this.selectedTower$ = this.store.select(selectCurrentTower);
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.towerService.towerData$.subscribe((towers) => this.store.dispatch(setTowers({towers})))
+  }
 
   onSelectTower(event: Event): void {
     const target = event.target as HTMLSelectElement;
