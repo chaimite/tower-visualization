56caa7ce39bdb2775345e4d7f8ad57bd6ca65571
xaimite <adrianocorreiacesar@gmail.com>
2025-04-15 08:42:31 +0100

    feat: enforce standalone because of error

--- tower-visualization/src/app/app.component.ts
+++ tower-visualization/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { TowerComponent } from './tower/tower.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  standalone: true,
   imports: [TowerComponent]
 })
 export class AppComponent {
