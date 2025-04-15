77d655bdf06f27f2e25ac67f9e87d27caca5c3c2
xaimite <adrianocorreiacesar@gmail.com>
2025-04-15 10:32:30 +0100

    test: fix broken tests and remove not needed ones

--- tower-visualization/src/app/app.component.spec.ts
+++ tower-visualization/src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { towerReducer } from './tower/tower.reducer';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, StoreModule.forRoot({ towers: towerReducer})],
     }).compileComponents();
   });
 
@@ -12,18 +14,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'tower-visualization' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('tower-visualization');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tower-visualization');
   });
 });
--- tower-visualization/src/app/tower/tower.component.spec.ts
+++ tower-visualization/src/app/tower/tower.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TowerComponent } from './tower.component';
+import { StoreModule } from '@ngrx/store';
 
 describe('TowerComponent', () => {
   let component: TowerComponent;
@@ -8,7 +9,7 @@ describe('TowerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TowerComponent]
+      imports: [TowerComponent, StoreModule.forRoot({})]
     })
     .compileComponents();
 
